fdae2c559a41678a902c343b0ab47d0d4fa81a29
Josefin Reuter <josefin@josefinreuter.fi>
2019-03-01 21:02:35 +0200

    Typescript setup

--- App.tsx
+++ App.tsx
@@ -1,51 +1,97 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, View, ImageBackground } from 'react-native';
+import { List, ListItem, Card, CardItem, Container, Header, Content, Footer, FooterTab, Button, Icon, Text, Title, Left, Right, Body } from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-interface Props {}
+type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+      <Container>
+        <ImageBackground source={require('./assets/images/bar2.jpg')} style={{ width: '100%', height: '100%' }}>
+          <Header>
+            <Left />
+            <Body>
+              <Title>WODIARY</Title>
+            </Body>
+            <Right />
+          </Header>
+
+          <Content style={styles.background}>
+            <Card>
+              <CardItem header bordered>
+                <Text>Workout Stats</Text>
+              </CardItem>
+              <CardItem bordered>
+                <Body>
+                  <Text>
+                    Test
+                  </Text>
+                </Body>
+              </CardItem>
+            </Card>
+
+            <Card >
+              <CardItem header bordered bordered>
+                <Text>Latest Workouts</Text>
+              </CardItem>
+              <CardItem>
+                <Body>
+                  <Text>
+                    Test
+                  </Text>
+                </Body>
+              </CardItem>
+            </Card>
+          </Content>
+
+          <Footer>
+            <FooterTab>
+              <Button vertical>
+                <Icon name="home" />
+              </Button>
+              <Button vertical>
+                <Icon name="add" />
+              </Button>
+              <Button vertical>
+                <Icon type="MaterialCommunityIcons" name="chart-line-variant" />
+              </Button>
+              <Button vertical>
+                <Icon active name="book" />
+              </Button>
+              <Button vertical>
+                <Icon name="ios-more" />
+              </Button>
+            </FooterTab>
+          </Footer>
+        </ImageBackground>
+      </Container>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    background: {
+        backgroundColor: 'rgba(242, 242, 242,.6)'
+    },
+    card: {
+        margin: 20
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
