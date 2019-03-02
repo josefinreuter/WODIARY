8600b56ad6b57838a493689f650b245a9c884e9e
Josefin Reuter <josefin@josefinreuter.fi>
2019-03-02 12:14:23 +0200

    minor changes

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import { List, ListItem, Card, CardItem, Container, Header, Content, Footer, Foo
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+          const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
     return (
       <Container>
-        <ImageBackground source={require('./assets/images/bar2.jpg')} style={{ width: '100%', height: '100%' }}>
+        <ImageBackground source={require('./src/assets/images/bar2.jpg')} style={{ width: '100%', height: '100%' }}>
           <Header>
             <Left />
             <Body>
@@ -19,24 +20,22 @@ export default class App extends Component<Props> {
           </Header>
 
           <Content style={styles.background}>
-            <Card>
-              <CardItem header bordered>
+            <Card  style={{ borderRadius: 8, borderMargin: 20, marginLeft: 10, marginRight: 10, marginTop: 15 }}>
+              <CardItem header bordered style={{ borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
                 <Text>Workout Stats</Text>
               </CardItem>
-              <CardItem bordered>
+              <CardItem bordered style={{ borderBottomLeftRadius: 8, borderBottomRightRadius: 8 }}>
                 <Body>
-                  <Text>
-                    Test
-                  </Text>
+                 
                 </Body>
               </CardItem>
             </Card>
 
-            <Card >
-              <CardItem header bordered bordered>
+            <Card style={{ borderRadius: 8, borderMargin: 20, marginLeft: 10, marginRight: 10, marginTop: 10 }}>
+              <CardItem header bordered style={{ borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
                 <Text>Latest Workouts</Text>
               </CardItem>
-              <CardItem>
+              <CardItem bordered style={{ borderBottomLeftRadius: 8, borderBottomRightRadius: 8 }}>
                 <Body>
                   <Text>
                     Test
@@ -77,21 +76,5 @@ const styles = StyleSheet.create({
     },
     card: {
         margin: 20
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    }
 });
